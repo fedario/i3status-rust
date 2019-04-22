34c8b35002cb5f5f14056e1afe30874cf6b5ff0a
fedario <dario-ramming@gmx.de>
2019-04-22 10:29:15 +0200

    added font awesome 5 support

--- src/icons.rs
+++ src/icons.rs
@@ -119,6 +119,66 @@ lazy_static! {
         "mouse" => " \u{f245}"
     };
 
+    pub static ref AWESOME_5: Map<String, String> = map_to_owned! {
+        "" => "",
+        "time" => " \u{f017} ",
+        "music" => " \u{f001} ",
+        "music_play" => "  \u{f04b}  ",
+        "music_pause" => "  \u{f04c}  ",
+        "music_next" => " \u{f061} ",
+        "music_prev" => " \u{f060} ",
+        "cogs" => " \u{f085} ",
+        "memory_mem" => " \u{f2db} ",
+        "memory_swap" => " \u{f0a0} ",
+        "cpu" => " \u{f3fd} ",
+        "bat" => " \u{f242} ",
+        "bat_full" => " \u{f240} ",
+        "bat_charging" => " \u{f1e6} ",
+        "bat_discharging" => " \u{f242} ",
+        "update" => " \u{f062} ",
+        "toggle_off" => " \u{f204} ",
+        "toggle_on" => " \u{f205} ",
+        // This icon has no spaces around it because it is manually set as text. (sound.rs)
+        "crossed" => "\u{f00d}",
+        "volume_full" => " \u{f028} ",
+        "volume_half" => " \u{f027} ",
+        "volume_empty" => " \u{f026} ",
+        "volume_muted" => " \u{f026} ",
+        "mic_full" => " \u{f130} ",
+        "mic_half" => " \u{f130} ",
+        "mic_empty" => " \u{f130} ",
+        "mic_muted" => " \u{f131} ",
+        "thermometer" => " \u{f2c8} ",
+        "xrandr" => " \u{f26c} ",
+        "net_up" => " \u{2b06} ",
+        "net_down" => " \u{2b07} ",
+        "net_wireless" => " \u{f1eb} ",
+        "net_wired" => " \u{f0ac} ",
+        "net_vpn" => " \u{f023} ",
+        "ping" => " \u{21ba} ",
+        "backlight_empty" => " \u{f0eb} ",
+        "backlight_partial1" => " \u{f0eb} ",
+        "backlight_partial2" => " \u{f0eb} ",
+        "backlight_partial3" => " \u{f0eb} ",
+        "backlight_full" => " \u{f0eb} ",
+        "weather_sun" => " \u{f185} ",
+        "weather_snow" => " \u{f2dc} ",
+        "weather_thunder" => " \u{f0e7} ",
+        "weather_clouds" => " \u{f0c2} ",
+        "weather_rain" => " \u{f043} ",
+        // Cloud symbol as default
+        "weather_default" => " \u{f0c2} ",
+        // Same as time symbol.
+        "uptime" => " \u{f017} ",
+        "gpu" => " \u{f26c} ",
+        "mail" => " \u{f0e0} ",
+        "bluetooth" => " \u{f294}",
+        "headphones" => " \u{f025}",
+        "joystick" => " \u{f11b}",
+        "keyboard" => " \u{f11c}",
+        "mouse" => " \u{f245}"
+    };
+
     pub static ref MATERIAL: Map<String, String> = map_to_owned! {
         "" => "",
         "time" => " \u{e192} ",
@@ -161,6 +221,7 @@ pub fn get_icons(name: &str) -> Option<Map<String, String>> {
     match name {
         "material" => Some(MATERIAL.clone()),
         "awesome" => Some(AWESOME.clone()),
+        "awesome 5" => Some(AWESOME_5.clone()),
         "none" => Some(NONE.clone()),
         _ => None,
     }
